e84268e8dda9e45a83b703c338acd334459a2e36
DESKTOP-QCQ8NDV\m_par <m_parhiz@yahoo.com>
2020-07-29 22:49:00 +1000

    configured store

---
+++ src/app/core/store/user/user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import * as fromUser from './reducers';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(fromUser.userFeatureKey, fromUser.reducers, { metaReducers: fromUser.metaReducers })
+  ]
+})
+export class UserModule { }
